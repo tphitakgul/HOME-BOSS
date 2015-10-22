4420583b9a0bd4906afa74fb45a3b0f46093e176
T.Phitakgul <t.phitakgul@gmail.com>
2015-10-23 03:54:24 +0700

    V. 1.0.1 inital

---
+++ Home Wireless/#Router-RED-DOIT-HOME#/Step-By-Step-Setting.bat
@@ -0,0 +1,85 @@
+@echo off
+color a
+
+cls
+
+echo SSID : blueoceanthroughlinksys
+echo =================== Setup Router START ===================
+ping -n 4 localhost>nul
+echo Connect Lan With your Computer
+
+echo ---------------------------------
+ping -n 4 localhost>nul
+echo Open url link 192.168.1.1
+pause
+start chrome 192.168.1.1
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Alert dialog
+ping -n 4 localhost>nul
+echo User : "admin"
+ping -n 4 localhost>nul
+echo Password : "admin"
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo ##### Menu => Setup => Basic Setup #####
+ping -n 4 localhost>nul
+echo Set Detail from picture "wireless-1.png"
+ping -n 4 localhost>nul
+echo Username : "69tkcs2cce@indy"
+ping -n 4 localhost>nul
+echo Password : "4bpsU4en"
+pause
+start wireless-1.png
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Scoll Down and Set Detail from picture "wireless-2.png"
+ping -n 4 localhost>nul
+pause
+start wireless-2.png
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Check Config 
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Save Settings
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Waiting a min  
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo - Note Router should used.
+ping -n 4 localhost>nul
+echo ##### Menu => Wireless => Wireless Security #####
+ping -n 4 localhost>nul
+echo Set Detail from picture "wireless-3.png"
+ping -n 4 localhost>nul
+echo Passphrase : "boss&beauty_wow"
+pause
+start wireless-3.png
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo ##### Menu => Wireless => Basic Wireless Settings #####
+ping -n 4 localhost>nul
+echo Set Detail from picture "wireless-4.jpg"
+ping -n 4 localhost>nul
+echo SSID : "blueoceanthroughlinksys"
+pause 
+start wireless-4.jpg
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Save Setting
+ping -n 4 localhost>nul
+echo --------------------------------- 
+ping -n 4 localhost>nul
+echo Done!
+echo =================== Setup Router END ===================
+pause
+exit
